8de485aa98460b3bb6ff4e58fc4f9620111927c1
jashan-sandhu21 <jashansd21@gmail.com>
2026-04-07 21:41:52 +0000

    hangman game

---
+++ hangman/hangman.py
@@ -0,0 +1,78 @@
+import random
+stages = [r"""  
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========''','''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========''','''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========''','''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''','''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========''','''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+========="""]
+end_of_game = False
+word_list = ["ardvark", "baboon", "camel"]
+chosen_word = random.choice(word_list)
+print(f"the solution is {chosen_word}")
+display = []
+word_length = len(chosen_word)
+lives = 6
+for _ in range(word_length):
+    display += "_"
+print(display)
+while not end_of_game:
+    guess = input("Guess letter: ").lower()
+    for position in range(word_length):
+        letter = chosen_word[position]
+        if letter == guess:
+            display[position]= letter
+    if guess not in chosen_word:
+        lives -= 1
+        if lives == 0:
+            end_of_game = True
+            print("You Lose !!!")
+    print(display)
+    print(stages[lives])
+    if "_" not in display:
+        end_of_game = True
+        print("You Win!!")
+    
