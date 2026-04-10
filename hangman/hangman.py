c2748b810f0468d7f4265d5a53eca28d6694ad55
jashan-sandhu21 <jashansd21@gmail.com>
2026-04-10 01:07:27 +0000

    hangman game

--- hangman/hangman.py
+++ hangman/hangman.py
@@ -11,12 +11,15 @@ for _ in range(word_length):
 print(display)
 end_of_game = False
 while not end_of_game:
-    guess = input("Guess a letter: ")
+    guess = input("Guess a letter: ").lower()
+    if guess in display:
+        print(f"you have already guessed {guess}")
     for position in range(word_length):
         letter = chosen_word[position]
         if letter == guess :
             display[position] = letter
     if guess not in chosen_word:
+        print(f"you have guessed {guess},that is not in the word. You lose a life.")
         lives -= 1
         if lives == 0:
             end_of_game = True 
